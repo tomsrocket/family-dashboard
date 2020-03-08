431188b1dc9229f55a9a3ef03b87a3550da65acf
Thomas Werner <thomas.w.werner@gmail.com>
2020-03-08 23:04:32 +0100

    Add special ourgroceries list with household timers

--- load-our-groceries.js
+++ load-our-groceries.js
@@ -1,11 +1,34 @@
-var OurGroceriesClient = require('our-groceries-client');
+//var OurGroceriesClient = require('our-groceries-client');
+
+var OurGroceriesClient = require('/home/thomas/git/our-groceries-client/src/our-groceries-client.js');
+
 // or import OurGroceriesClient from 'our-groceries-client' 
 const fs = require('fs') 
 const config = require('./config.json')
 
 var username = config.our_groceries.username
   , password = config.our_groceries.password
-  , ourLists = config.our_groceries.lists;
+  , ourLists = config.our_groceries.lists
+  , listWithTimers = config.our_groceries.listWithTimers;
+
+var oldTimers = {};
+if (listWithTimers) {
+    try {
+        oldTimersList = require('./public/cal/'+(listWithTimers.toLowerCase())+'.json')
+        console.log("Using existing timers from list " + listWithTimers);
+        oldTimersList.forEach(function(item, index) {
+            oldTimers[item.id] = item;
+        })
+    }
+    catch (e) {
+        console.log('No existing timers file found')
+        console.log(e)
+    }
+
+}
+
+const today = new Date();
+var todayString = today.toISOString().substr(0,10);
 
 var client = new OurGroceriesClient();
 
@@ -36,31 +59,40 @@ function getList(listName) {
         getListComplete: function(filename) {
             return function(result) {
             var list = result.response.list;
-//            console.log(list);
 
             var items = [];
             list.items.forEach(function(item, index) {
-                if (!item.crossedOff) {
+
+                // There can be one special list (name: config.listWithTimers)
+                // and if there are items in that list, 
+                // then they will be automatically crossed off, and the date when the item was crossed off will be saved in the hash
+                if (listWithTimers && (listWithTimers == listName)) {
+                    if (item.crossedOff) {
+                        item["date"] = oldTimers[item.id].date
+                    } else {
+                        client.crossOff(item.id, list.id, handlers.crossOffComplete);
+                        item["date"] = todayString;    
+                    }
                     items.push(item);
+
+                // All other lists behave normally and only have the non-crossedOff items
+                } else {
+                    if (!item.crossedOff) {
+                        items.push(item);
+                    }
                 }
             })
             console.log("-------ITEMS of "+listName+"-----------")
             console.log(items)
 
-            // Write data in 'Output.txt' . 
-            fs.writeFile('public/cal/'+filename+'.json', JSON.stringify(items), (err) => { 
-                
-                // In case of a error throw err. 
-                if (err) throw err; 
-            }) 
-
-
-            //client.addToList(list.id, itemName, quantity, handlers.addToListComplete);
+            fs.writeFile('public/cal/'+filename+'.json', JSON.stringify(items), (err) => {                
+                if (err) throw err;
+            })
         }},
-        // Called after adding the item completes
-        addToListComplete: function(result) {
+
+        crossOffComplete: function(result) {
             if (result.success) {
-                console.log("Successfully added to list.");
+                console.log("Successfully crossed off.");
             } else {
                 console.log("Unable to add to list: "+result.error);
             }
