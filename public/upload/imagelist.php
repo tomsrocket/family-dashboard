c0d0db33e828c764d3ff062f36f4c15172ffffb7
Thomas Werner <thomas.w.werner@gmail.com>
2026-03-31 18:17:23 +0200

    Resize preview images

--- public/upload/imagelist.php
+++ public/upload/imagelist.php
@@ -1,22 +1,74 @@
 <?php
 
 $dir = __DIR__ . '/../photofeed/';
-$files = array_diff(scandir($dir), ['.', '..']);
+$cacheDir = $dir . 'cache/';
+
+if (!is_dir($cacheDir)) {
+    mkdir($cacheDir, 0777, true);
+}
+
+$files = array_diff(scandir($dir), ['.', '..', 'cache']);
 
 $images = [];
 
 foreach ($files as $file) {
-    if (preg_match('/\.(jpg|jpeg|png)$/i', $file)) {
-        if ($file == "latest.jpg") continue; 
-        $images[] = [
-            "url" => "/photofeed/" . $file,
-            "time" => filemtime($dir . $file)
-        ];
+
+    if (!preg_match('/\.(jpg|jpeg|png)$/i', $file)) continue;
+
+    $originalPath = $dir . $file;
+    $cachePath = $cacheDir . $file;
+
+    // 👉 Thumbnail erzeugen, wenn noch nicht vorhanden
+    if (!file_exists($cachePath)) {
+
+        $info = getimagesize($originalPath);
+        if (!$info) continue;
+
+        [$width, $height, $type] = $info;
+
+        // max 300px
+        $maxSize = 300;
+        $scale = min($maxSize / $width, $maxSize / $height, 1);
+
+        $newWidth = (int)($width * $scale);
+        $newHeight = (int)($height * $scale);
+
+        // Bild laden
+        switch ($type) {
+            case IMAGETYPE_JPEG:
+                $src = imagecreatefromjpeg($originalPath);
+                break;
+            case IMAGETYPE_PNG:
+                $src = imagecreatefrompng($originalPath);
+                break;
+            default:
+                continue 2;
+        }
+
+        $dst = imagecreatetruecolor($newWidth, $newHeight);
+
+        imagecopyresampled(
+            $dst, $src,
+            0, 0, 0, 0,
+            $newWidth, $newHeight,
+            $width, $height
+        );
+
+        // speichern
+        imagejpeg($dst, $cachePath, 70);
+
+        imagedestroy($src);
+        imagedestroy($dst);
     }
+
+    $images[] = [
+        "url" => "/photofeed/cache/" . $file,
+        "time" => filemtime($originalPath)
+    ];
 }
 
 // neueste zuerst
 usort($images, fn($a, $b) => $b['time'] - $a['time']);
 
 header('Content-Type: application/json');
-echo json_encode(array_slice($images, 0, 50)); // nur die neuesten Bilder zurückgeben
+echo json_encode($images);
