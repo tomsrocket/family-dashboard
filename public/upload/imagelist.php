c411eed7187584c5c4b5796a0584630386867ec5
Thomas Werner <thomas.w.werner@gmail.com>
2026-03-31 18:59:45 +0200

    respect EXIF orientation

--- public/upload/imagelist.php
+++ public/upload/imagelist.php
@@ -7,6 +7,67 @@ if (!is_dir($cacheDir)) {
     mkdir($cacheDir, 0777, true);
 }
 
+
+
+function loadImageCorrectOrientation($path) {
+
+    $info = getimagesize($path);
+    [$width, $height, $type] = $info;
+    // Bild laden
+    switch ($type) {
+        case IMAGETYPE_JPEG:
+            $img = imagecreatefromjpeg($path);
+            break;
+        case IMAGETYPE_PNG:
+            $img = imagecreatefrompng($path);
+            break;
+        default:
+            echo "unknown image type: $type";
+            return null;
+    }
+
+    $exif = @exif_read_data($path);
+
+    if (!empty($exif['Orientation'])) {
+
+        switch ($exif['Orientation']) {
+
+            case 2:
+                imageflip($img, IMG_FLIP_HORIZONTAL);
+                break;
+
+            case 3:
+                $img = imagerotate($img, 180, 0);
+                break;
+
+            case 4:
+                imageflip($img, IMG_FLIP_VERTICAL);
+                break;
+
+            case 5:
+                $img = imagerotate($img, -90, 0);
+                imageflip($img, IMG_FLIP_HORIZONTAL);
+                break;
+
+            case 6:
+                $img = imagerotate($img, -90, 0);
+                break;
+
+            case 7:
+                $img = imagerotate($img, 90, 0);
+                imageflip($img, IMG_FLIP_HORIZONTAL);
+                break;
+
+            case 8:
+                $img = imagerotate($img, 90, 0);
+                break;
+        }
+    }
+
+    return $img;
+}
+
+
 $files = array_diff(scandir($dir), ['.', '..', 'cache']);
 
 $images = [];
@@ -15,35 +76,27 @@ foreach ($files as $file) {
 
     if (!preg_match('/\.(jpg|jpeg|png)$/i', $file)) continue;
 
+    if ($file === 'latest.jpg') continue;
+    
     $originalPath = $dir . $file;
     $cachePath = $cacheDir . $file;
 
     // 👉 Thumbnail erzeugen, wenn noch nicht vorhanden
     if (!file_exists($cachePath)) {
 
-        $info = getimagesize($originalPath);
-        if (!$info) continue;
+        $src = loadImageCorrectOrientation($originalPath);
+        if (!$src) continue;
 
-        [$width, $height, $type] = $info;
+        // 👉 NACH Rotation!
+        $width = imagesx($src);
+        $height = imagesy($src);
 
-        // max 300px
         $maxSize = 300;
+
         $scale = min($maxSize / $width, $maxSize / $height, 1);
 
         $newWidth = (int)($width * $scale);
         $newHeight = (int)($height * $scale);
-
-        // Bild laden
-        switch ($type) {
-            case IMAGETYPE_JPEG:
-                $src = imagecreatefromjpeg($originalPath);
-                break;
-            case IMAGETYPE_PNG:
-                $src = imagecreatefrompng($originalPath);
-                break;
-            default:
-                continue 2;
-        }
 
         $dst = imagecreatetruecolor($newWidth, $newHeight);
 
