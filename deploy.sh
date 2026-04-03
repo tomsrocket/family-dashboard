c2259427593b65f8cd4f1154b922178ca5b0e5a1
Thomas Werner <thomas.w.werner@gmail.com>
2026-04-03 21:44:11 +0200

    update the scripts

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,7 @@ REMOTEPATH=$(jq -r '.deploy.remotepath' config.json)
 echo "Remote path ist: $REMOTEPATH"
 
 read -p "There should not be a trailing slash. Press enter to continue"
-rsync -azPdv ./load-* "$REMOTEPATH"
-rsync -azPdv public/ "$REMOTEPATH/public"
+rsync -azPdv *.sh "$REMOTEPATH"
+rsync -azPdv *.js "$REMOTEPATH"
+rsync -azPdv --exclude="photofeed" public/ "$REMOTEPATH/public"
+
